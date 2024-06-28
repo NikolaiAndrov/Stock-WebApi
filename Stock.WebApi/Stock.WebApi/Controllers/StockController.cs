5b8d6d8c2b0ec7be8eed615c946afdcf5d69d3fa
nikolaiandrov <nikolaiandrov@abv.bg>
2024-06-28 13:11:09 +0300

    Stock controller created and 1 method just for testing purposes

---
+++ Stock.WebApi/Stock.WebApi/Controllers/StockController.cs
@@ -0,0 +1,28 @@
+﻿namespace Stock.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Stock.Data;
+    using Stock.Data.Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly StockDbContext dbContext;
+
+        public StockController(StockDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            ICollection<Stock> stocks = await this.dbContext.Stocks.ToListAsync();
+
+            return this.Ok(stocks);
+        }
+    }
+}
