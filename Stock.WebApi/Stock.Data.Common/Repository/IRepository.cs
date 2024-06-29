b488a008b4dc463a9b7b355ae3a0cf6200761c24
nikolaiandrov <nikolaiandrov@abv.bg>
2024-06-29 13:20:27 +0300

    all methods in repo implemented

--- Stock.WebApi/Stock.Data.Common/Repository/IRepository.cs
+++ Stock.WebApi/Stock.Data.Common/Repository/IRepository.cs
@@ -2,5 +2,8 @@
 {
     public interface IRepository
     {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadonly<T>() where T : class;
     }
 }
--- Stock.WebApi/Stock.Data.Common/Repository/Repository.cs
+++ Stock.WebApi/Stock.Data.Common/Repository/Repository.cs
@@ -1,6 +1,23 @@
 ﻿namespace Stock.Data.Common.Repository
 {
+    using Microsoft.EntityFrameworkCore;
+
     public class Repository : IRepository
     {
+        private readonly StockDbContext dbContext;
+
+        public Repository(StockDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+            => this.dbContext.Set<T>();
+
+        public IQueryable<T> All<T>() where T : class
+            => this.DbSet<T>();
+
+        public IQueryable<T> AllReadonly<T>() where T : class
+            => this.DbSet<T>().AsNoTracking();
     }
 }
