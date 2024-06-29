95a871be578659b51aa759841e307f2b0a9a5cb2
nikolaiandrov <nikolaiandrov@abv.bg>
2024-06-29 14:56:42 +0300

    AddAsync, Delete, SaveChangesAsync, methods implemented in the Repository

--- Stock.WebApi/Stock.Data.Common/Repository/IRepository.cs
+++ Stock.WebApi/Stock.Data.Common/Repository/IRepository.cs
@@ -5,5 +5,11 @@
         IQueryable<T> All<T>() where T : class;
 
         IQueryable<T> AllReadonly<T>() where T : class;
+
+        Task AddAsync<T>(T  entity) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        Task<int> SaveChangesAsync();
     }
 }
--- Stock.WebApi/Stock.Data.Common/Repository/Repository.cs
+++ Stock.WebApi/Stock.Data.Common/Repository/Repository.cs
@@ -1,6 +1,7 @@
 ﻿namespace Stock.Data.Common.Repository
 {
     using Microsoft.EntityFrameworkCore;
+    using System.Threading.Tasks;
 
     public class Repository : IRepository
     {
@@ -19,5 +20,14 @@
 
         public IQueryable<T> AllReadonly<T>() where T : class
             => this.DbSet<T>().AsNoTracking();
+
+        public async Task AddAsync<T>(T entity) where T : class
+            => await this.DbSet<T>().AddAsync(entity); 
+
+        public void Delete<T>(T entity) where T : class
+            => this.DbSet<T>().Remove(entity);
+
+        public async Task<int> SaveChangesAsync()
+            => await this.dbContext.SaveChangesAsync();
     }
 }
