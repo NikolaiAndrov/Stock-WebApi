25acab7831a5ccf1933df31126a779b4ecb286fa
nikolaiandrov <nikolaiandrov@abv.bg>
2024-07-01 14:13:54 +0300

    CommentController added

---
+++ Stock.WebApi/Stock.WebApi/Controllers/CommentController.cs
@@ -0,0 +1,17 @@
+﻿namespace Stock.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Stock.Services.Interfaces;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+    }
+}
