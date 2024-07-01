ef851729d295d59669eda373d3f6b17f145749ac
nikolaiandrov <nikolaiandrov@abv.bg>
2024-07-01 14:33:21 +0300

    comment mapping class created

---
+++ Stock.WebApi/Stock.WebApi.Infrastructure/Extensions/CommentMappers.cs
@@ -0,0 +1,22 @@
+﻿namespace Stock.WebApi.Infrastructure.Extensions
+{
+    using Stock.Data.Models;
+    using Stock.WebApi.DtoModels.Comment;
+
+    public static class CommentMappers
+    {
+        public static CommentDto ToCommentDto(this Comment comment)
+        {
+            CommentDto commentDto = new CommentDto
+            {
+                Id = comment.Id,
+                Title = comment.Title,
+                Content = comment.Content,
+                CreatedOn = comment.CreatedOn,
+                StockId = comment.StockId
+            };
+
+            return commentDto;
+        }
+    }
+}
