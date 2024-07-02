96867c1b52fd17a2276741bf6dd2c7cdcbe97533
nikolaiandrov <nikolaiandrov@abv.bg>
2024-07-02 13:07:41 +0300

    extension method created to map Comment from CreateCommentDto

--- Stock.WebApi/Stock.WebApi.Infrastructure/Extensions/CommentMappers.cs
+++ Stock.WebApi/Stock.WebApi.Infrastructure/Extensions/CommentMappers.cs
@@ -18,5 +18,17 @@
 
             return commentDto;
         }
+
+        public static Comment ToComment(this CreateCommentDto createCommentDto, int stockId)
+        {
+            Comment comment = new Comment
+            {
+                Title = createCommentDto.Title,
+                Content = createCommentDto.Content,
+                StockId = stockId
+            };
+
+            return comment;
+        }
     }
 }
