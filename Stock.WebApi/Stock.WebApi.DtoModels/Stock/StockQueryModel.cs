b1e3ccbae106bc5e9aed24931aabe176b0271f93
nikolaiandrov <nikolaiandrov@abv.bg>
2024-07-04 14:25:22 +0300

    code refactoring

--- Stock.WebApi/Stock.WebApi.DtoModels/Stock/StockQueryModel.cs
+++ Stock.WebApi/Stock.WebApi.DtoModels/Stock/StockQueryModel.cs
@@ -2,7 +2,6 @@
 {
     using System.ComponentModel.DataAnnotations;
     using static Common.EntityValidationConstants.StockValidation;
-    using static Common.ApplicationErrorMessages;
 
     public class StockQueryModel
     {
