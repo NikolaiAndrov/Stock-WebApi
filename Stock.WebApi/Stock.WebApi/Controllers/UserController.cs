8870076fef828eacd3d8c6364e72e43894982954
nikolaiandrov <nikolaiandrov@abv.bg>
2024-07-05 15:20:58 +0300

    UserController added

---
+++ Stock.WebApi/Stock.WebApi/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿namespace Stock.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Stock.Data.Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+    }
+}
