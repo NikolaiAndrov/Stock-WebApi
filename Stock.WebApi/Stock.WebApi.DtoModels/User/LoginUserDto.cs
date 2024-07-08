2c21f3ab685bfd2cbec2fdd5468c407ad31ee608
nikolaiandrov <nikolaiandrov@abv.bg>
2024-07-08 15:28:16 +0300

    LoginUserDto created

---
+++ Stock.WebApi/Stock.WebApi.DtoModels/User/LoginUserDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Stock.WebApi.DtoModels.User
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Common.EntityValidationConstants.UserValidation;
+
+    public class LoginUserDto
+    {
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
