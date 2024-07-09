d3a6c46c877ec232ce3a91a85c0726b3c98fa60b
nikolaiandrov <nikolaiandrov@abv.bg>
2024-07-09 14:47:12 +0300

    PortfolioController added

---
+++ Stock.WebApi/Stock.WebApi/Controllers/PortfolioController.cs
@@ -0,0 +1,16 @@
+﻿namespace Stock.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PortfolioController : ControllerBase
+    {
+        public PortfolioController()
+        {
+            
+        }
+    }
+}
