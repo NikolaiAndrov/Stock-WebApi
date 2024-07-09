13d4f7a96c8c334b8c3da92adf44c9a27d3a63d1
nikolaiandrov <nikolaiandrov@abv.bg>
2024-07-09 14:53:09 +0300

    extension method for getting user id added

---
+++ Stock.WebApi/Stock.WebApi.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Stock.WebApi.Infrastructure.Extensions
+{
+    using System.Security.Claims;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetId(this ClaimsPrincipal user)
+        {
+            string? id = user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return id;
+        }
+    }
+}
